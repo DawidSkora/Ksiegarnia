ba49538fbaf3d4e8aaceca9da9e1b2edb89fa3b3
niuuby <patryk19952009@o2.pl>
2021-01-20 16:24:18 +0100

    przegladanie oferty

--- formStronaGlowna.cs
+++ formStronaGlowna.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 
 // Czcionka uzywana do tekstow: Modern No. 20
-// 
+// Kolor background: Active Caption
 
 
 namespace Ksiegarnia
@@ -27,24 +27,7 @@ namespace Ksiegarnia
             timerStronaGlowna.Enabled = true; // podczas pierwszego ładowania strony uruchamiamy timer, który jest timerem pomocniczym
         }
 
-        public static class ZmienneGlobalne // zmienne globalne mają za zadanie służyć w dowolnym miejscu kodu
-        {
-            private static bool czy_zalogowany = false; // zmienna bool do przechowywania informacji 
-            public static bool czy_zalogowany_get_set // getter i setter 
-            {
-                get { return czy_zalogowany; }
-                set { czy_zalogowany = value; }
-            }
 
-            private static bool czy_admin = false; // podobnie jak powyżej, w tym przypadku informacja czy mamy prawa admina
-            public static bool czy_admin_get_set // i rowniez getter i setter
-            {
-                get { return czy_admin; }
-                set { czy_admin = value; }
-            }
-
-
-        }
 
         private void timerStronaGlowna_tick(object sender, EventArgs e)  // event timera ktory wspiera prace programu
         {
@@ -77,7 +60,8 @@ namespace Ksiegarnia
         // event wykonujacy sie po kliknieciu na przegladanie oferty w menu
         private void przegladanieOfertyToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            ZmienneGlobalne.formaPrzegladanieOferty_get_set.Visible = true;
+            this.Visible = false;
         }
 
         // event wykonujacy sie po kliknieciu na Koszyk w menu
@@ -89,9 +73,59 @@ namespace Ksiegarnia
         // event wykonujacy sie po kliknieciu na przycisk Przegladania oferty
         private void buttonPrzegladanieOferty_Click(object sender, EventArgs e)
         {
-
+            ZmienneGlobalne.formaPrzegladanieOferty_get_set.Visible = true;
+            this.Visible = false;
         }
 
        
+    }
+
+    public static class ZmienneGlobalne // zmienne globalne mają za zadanie służyć w dowolnym miejscu kodu
+    {
+        private static bool czy_zalogowany = false; // zmienna bool do przechowywania informacji 
+        public static bool czy_zalogowany_get_set // getter i setter 
+        {
+            get { return czy_zalogowany; }
+            set { czy_zalogowany = value; }
+        }
+
+        private static bool czy_admin = false; // podobnie jak powyżej, w tym przypadku informacja czy mamy prawa admina
+        public static bool czy_admin_get_set // i rowniez getter i setter
+        {
+            get { return czy_admin; }
+            set { czy_admin = value; }
+        }
+
+        private static formStronaGlowna formaStronaGlowna = new formStronaGlowna();
+        public static formStronaGlowna formaStronaGlowna_get_set
+        {
+            get { return formaStronaGlowna; }
+            set { formaStronaGlowna = value; }
+        }
+
+        private static formPrzegladanieOferty formaPrzegladanieOferty = new formPrzegladanieOferty();
+
+        public static formPrzegladanieOferty formaPrzegladanieOferty_get_set
+        {
+            get { return formaPrzegladanieOferty; }
+            set { formaPrzegladanieOferty = value; }
+        }
+
+    }
+
+    public class Ksiazka
+    {
+        public string Autor { get; }
+        public string Tytul { get; }
+        public int Cena { get; } // should not be a part of book, but I added it for simplicity reasons
+
+        public Ksiazka(string tytul, string autor, int cena)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Cena = cena;
+        }
+
+
     }
 }
